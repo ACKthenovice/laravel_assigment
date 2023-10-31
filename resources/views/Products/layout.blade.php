a007b6d46d6da74e94763aa9a9cd10501244c03b
ACKthenovice <samuelayechanko@gmail.com>
2023-10-31 15:24:11 +0630

    form adding

--- resources/views/Products/layout.blade.php
+++ resources/views/Products/layout.blade.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Products</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <h1>Products list</h1>
     <div class="container">
         @yield('content')
     </div>
